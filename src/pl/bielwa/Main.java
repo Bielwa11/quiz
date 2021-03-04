e96f06ea16767af6cd6b59cf71fd7a49d2eea716
Bielwa11 <bielwa10.bielejewski@gmail.com>
2021-03-04 10:50:03 +0100

    master

--- src/pl/bielwa/Main.java
+++ src/pl/bielwa/Main.java
@@ -6,71 +6,11 @@ public class Main {
     private Object scores;
 
     public static void main(String[] args) {
-        
-        NewQuiz quizz new NewQuiz();
-        quizz,initiateQuiz();
-        
-        Scanner scanner = new Scanner(System.in);
-        Map<String, String> dictionary = new HashMap<>(Map.of("arrow", "strzała", "shop", "sklep", "phone", "telefon", "accept", "akceptować", "after", "później", "add", "dodawać", "air", "powietrze", "machine", "maszyna", "meat", "mięso", "never", "nigdy"));
-        dictionary.putIfAbsent("book", "książka");
-        dictionary.putIfAbsent("continue", "kontynuować");
-        dictionary.putIfAbsent("cool", "chłodny");
-        dictionary.putIfAbsent("definition", "definicja");
-        dictionary.putIfAbsent("factory", "fabryka");
-        System.out.println("Cześć, jak masz na imię");
-        String nameUser = scanner.nextLine();
-        System.out.println("Cześć" + " " + nameUser);
 
+        NewQuiz quizz = new NewQuiz();
+        quizz.initiateQuiz();
 
-        while (true) {
-            System.out.println("Jeśli klikniesz 1 rozpoczniesz quiz z tłumczenia słowek");
-            System.out.println("Najwyższy wynik quizu to ");
-            System.out.println("Jeśli klikniesz 2 zamkniesz program");
-            String option = scanner.nextLine();
-            if (option.equals("1")) {
-                initiateQuiz(dictionary);
-            } else if (option.equals("2")) {
-                break;
-            }
-
-        }
     }
-
-    private initiateQuiz() {
-    }
-
-    private static void initiateQuiz(Map<String, String> dictionary) {
-        Scanner quizScanner = new Scanner(System.in);
-        List<String> keys = new ArrayList<>(dictionary.keySet());
-        Random random = new Random();
-        int questionIndex = 0;
-        int score = 0;
-        while (questionIndex < 15) {
-            int index = random.nextInt(keys.size());
-            String word = keys.get(index);
-            System.out.println("(Pytanie " + (questionIndex + 1) + "/15)");
-            System.out.println("Przetłumacz słowo " + word);
-            String wordFromUser = quizScanner.nextLine();
-
-
-            if (wordFromUser.equalsIgnoreCase(dictionary.get(word))) {
-                System.out.println("Zgadza się!");
-                score++;
-            } else {
-                System.out.println("NIESTETY blad...");
-            }
-            questionIndex++;
-        }
-        System.out.println("Twój wynik to " + score + "/15");
-        double finalScore = score / questionIndex;
-        if (score >= 10) {
-            System.out.println("Bardzo dobrze!" + finalScore + "% poprawych odpowiedzi");
-        } else {
-            System.out.println("Niestety! Tylko " + finalScore + "% poprawnych odpowiedzi");
-        }
-    }
-}
-
 
 
 
--- src/pl/bielwa/NewQuiz.java
+++ src/pl/bielwa/NewQuiz.java
@@ -1,15 +1,166 @@
 package pl.bielwa;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+import java.util.Scanner;
+
 public class NewQuiz {
 
+    public static final double GOOD_ENOUGH_NUMBER = 60.0;
 
+    Random random = new Random();
+
+    Scanner scanner = new Scanner(System.in);
+
+    Map<String, String> dictionary = new HashMap<>(Map.of("arrow", "strzała", "shop", "sklep", "phone", "telefon", "accept", "akceptować", "after", "później", "add", "dodawać", "air", "powietrze", "machine", "maszyna", "meat", "mięso", "never", "nigdy"));
+
+    public static final int QUESTIONS_NUMBER = 15;
+
+
+    public NewQuiz() {
+
+        fillMap();
+
+    }
+
+
+    public void fillMap() {
+
+        dictionary.putIfAbsent("book", "książka");
+
+        dictionary.putIfAbsent("continue", "kontynuować");
+
+        dictionary.putIfAbsent("cool", "chłodny");
+
+        dictionary.putIfAbsent("definition", "definicja");
+
+        dictionary.putIfAbsent("factory", "fabryka");
+
+    }
+
+
+    public void initiate() {
+
+        greetings();
+
+        showMenu();
+
+    }
+
+
+    private void greetings() {
+
+        System.out.println("Cześć, jak masz na imię");
+
+        String nameUser = scanner.nextLine();
+
+        System.out.println("Cześć" + " " + nameUser);
+
+    }
+
+
+    private void showMenu() {
+
+        while (true) {
+
+            printOptions();
+
+            String option = scanner.nextLine();
+
+            if (option.equals("1")) {
+
+                runQuiz(dictionary);
+
+            } else if (option.equals("2")) {
+
+                break;
+
+            }
+
+        }
+
+    }
+
+
+    private void printOptions() {
+
+        System.out.println("Jeśli klikniesz 1 rozpoczniesz quiz z tłumczenia słowek");
+
+        System.out.println("Najwyższy wynik quizu to ");
+
+        System.out.println("Jeśli klikniesz 2 zamkniesz program");
+
+    }
+
+
+    private void runQuiz(Map<String, String> dictionary) {
+
+        List<String> keys = new ArrayList<>(dictionary.keySet());
+
+
+        int questionIndex = 0;
+
+        int score = 0;
+
+        while (questionIndex < QUESTIONS_NUMBER) {
+
+            String word = getRandomWord(keys);
+
+            System.out.println("(Pytanie " + (questionIndex + 1) + "/)" + QUESTIONS_NUMBER);
+
+            System.out.println("Przetłumacz słowo " + word);
+
+            String wordFromUser = scanner.nextLine();
+
+
+            if (wordFromUser.equalsIgnoreCase(dictionary.get(word))) {
+
+                System.out.println("Zgadza się!");
+
+                score++;
+
+            } else {
+
+                System.out.println("NIESTETY blad...");
+
+            }
+
+            questionIndex++;
+
+        }
+
+        System.out.println("Twój wynik to " + score + "/" + QUESTIONS_NUMBER);
+
+        double finalScore = score / questionIndex;
+
+        if (finalScore >= GOOD_ENOUGH_NUMBER) {
+
+            System.out.println("Bardzo dobrze!" + finalScore + "% poprawych odpowiedzi");
+
+        } else {
+
+            System.out.println("Niestety! Tylko " + finalScore + "% poprawnych odpowiedzi");
+
+        }
+
+    }
+
+
+    private String getRandomWord(List<String> keys) {
+
+        int index = random.nextInt(keys.size());
+
+        String word = keys.get(index);
+
+        return word;
+
+    }
+
+    public void initiateQuiz() {
+    }
 }
 
-    public void {
 
-    }
-
-    private void {
-
-
-    }
