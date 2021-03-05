755852890edc371946248cc85fa88bcc7e942bd9
Bielwa11 <bielwa10.bielejewski@gmail.com>
2021-03-05 10:51:03 +0100

    master

--- src/games/NewQuiz.java
+++ src/games/NewQuiz.java
@@ -1,4 +1,4 @@
-package pl.bielwa;
+package games;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -8,113 +8,112 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class NewQuiz {
-
-    public static final double SCORE_GOOD_ENOUGH = 60.0;
-
-    Random random = new Random();
-    Scanner scanner = new Scanner(System.in);
-    Map<String, String> dictionary = new HashMap<>(Map.of("arrow", "strzała", "shop", "sklep", "phone", "telefon", "accept", "akceptować", "after", "później", "add", "dodawać", "air", "powietrze", "machine", "maszyna", "meat", "mięso", "never", "nigdy"));
-    public static final int QUESTIONS_NUMBER = 16;
-    private Object finalScore;
-    double highScore;
+    private static final double SCORE_GOOD_ENOUGH = 60.0;
+    private Random random = new Random();
+    private Scanner scanner = new Scanner(System.in);
+    private Map<String, String> dictionary = new HashMap<>();
+    private static final int QUESTIONS_NUMBER = 16;
+    private double highScore = 0.0;
 
     public NewQuiz() {
-
         fillMap();
-
     }
 
 
     public void fillMap() {
-
+        dictionary.putIfAbsent("never", "nigdy");
+        dictionary.putIfAbsent("meat", "mięso");
+        dictionary.putIfAbsent("machine", "maszyna");
+        dictionary.putIfAbsent("air", "powietrze");
+        dictionary.putIfAbsent("add", "dodawać");
+        dictionary.putIfAbsent("after", "później");
+        dictionary.putIfAbsent("accept", "akceptować");
+        dictionary.putIfAbsent("phone", "telefon");
+        dictionary.putIfAbsent("shop", "sklep");
+        dictionary.putIfAbsent("arrow", "strzała");
         dictionary.putIfAbsent("book", "książka");
         dictionary.putIfAbsent("continue", "kontynuować");
         dictionary.putIfAbsent("cool", "chłodny");
         dictionary.putIfAbsent("definition", "definicja");
         dictionary.putIfAbsent("factory", "fabryka");
         dictionary.putIfAbsent("bike", "rower");
-
     }
 
 
     public void initiate() {
-
         greetings();
         showMenu();
-
     }
 
 
     private void greetings() {
-
         System.out.println("Cześć, jak masz na imię");
         String nameUser = scanner.nextLine();
         System.out.println("Cześć " + nameUser);
-
     }
 
 
     private void showMenu() {
-
         while (true) {
-
             printOptions();
             String option = scanner.nextLine();
             if (option.equals("1")) {
-
                 runQuiz(dictionary);
-
             } else if (option.equals("2")) {
-
                 break;
-
             }
-
         }
-
     }
 
 
     private void printOptions() {
         System.out.println("Jeśli klikniesz 1 rozpoczniesz quiz z tłumczenia słowek");
-        System.out.println("Najwyższy wynik quizu to " + finalScore);
+        System.out.println("Najwyższy wynik quizu to " + highScore);
         System.out.println("Jeśli klikniesz 2 zamkniesz program");
-
     }
 
 
     private void runQuiz(Map<String, String> dictionary) {
-
         List<String> keys = new ArrayList<>(dictionary.keySet());
         int questionIndex = 0;
         int score = 0;
 
         while (questionIndex < QUESTIONS_NUMBER) {
-
             String word = getRandomWord(keys);
             System.out.println("(Pytanie " + (questionIndex + 1) + "/" + QUESTIONS_NUMBER + ")");
             System.out.println("Przetłumacz słowo " + word);
             String wordFromUser = scanner.nextLine();
 
-
             if (wordFromUser.equalsIgnoreCase(dictionary.get(word))) {
                 System.out.println("Zgadza się!");
                 score++;
-                dictionary.remove(dictionary.get(word));
+                keys.remove(word);
             } else {
                 System.out.println("NIESTETY blad...");
             }
             questionIndex++;
-
         }
-
         System.out.println("Twój wynik to " + score + "/" + QUESTIONS_NUMBER);
         double finalScore = (double) score / (double) questionIndex * 100;
-        double highScore;
         if (finalScore >= SCORE_GOOD_ENOUGH) {
             System.out.println("Bardzo dobrze!" + finalScore + "% poprawych odpowiedzi");
         } else {
             System.out.println("Niestety! Tylko " + finalScore + "% poprawnych odpowiedzi");
+        }
+        saveHighScoreIfNeeded(finalScore);
+    }
+
+    private void saveHighScoreIfNeeded(double finalScore) {
+        if(Double.compare(finalScore,highScore)>0){
+            highScore = finalScore;
+        }
+        else{
+            //nic sie nie dzieje, highscore zostaje taki sam.
+            //przyklad komentarza
+
+            /*
+            przyklad komentarza
+             */
         }
     }
 
@@ -124,12 +123,6 @@ public class NewQuiz {
         String word = keys.get(index);
         return word;
     }
-
-    public <highScore, finalScore> void compare(finalScore, highScore) {
-        System.out.println(Integer.compare((int) finalScore, (int) highScore));
-    }
-
-
 }
 
 
--- src/pl/bielwa/Main.java
+++ src/pl/bielwa/Main.java
@@ -1,13 +1,13 @@
 package pl.bielwa;
 
+import games.NewQuiz;
+
 public class Main {
 
     public static void main(String[] args) {
 
         NewQuiz quizz = new NewQuiz();
-        quizz.fillMap();
         quizz.initiate();
-        quizz.compare();
     }
 }
 
